ec910dc125928ac2ceea7df34ed3da7ba4b5edb7
henriquearaujo91 <henrique_araujo91@outlook.com>
2019-06-08 01:45:40 -0300

    internacionalização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
@@ -40,7 +40,7 @@ import { ShoppingCarService } from './restaurant-detail/shopping-car/shopping-ca
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService, ShoppingCarService],
+  providers: [RestaurantsService, ShoppingCarService, { provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
